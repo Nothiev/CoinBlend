18e71012b3a2e14076ec1e69e84e2adac7b621d8
Arnaud THIEVIN <arnaud.thievin@efrei.net>
2024-05-16 17:04:57 +0200

    Repare build

--- dataiku.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Chargement des données
-data = pd.read_csv('C:/Users/arnau/Downloads/data_prepared.csv')
-
-# Vérification des types de données
-data.head(10)
--- save.col.py
+++
@@ -1,3 +0,0 @@
-# Sauvegarder les colonnes du modèle
-model_columns = list(X.columns)
-joblib.dump(model_columns, 'model_columns.pkl')
